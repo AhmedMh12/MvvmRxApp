76ba3e8ca1ad186d417b1cb9ed924ff2bc2adaf7
Ahmed Mh <ahmed.mahouchi@machinestalk.com>
2022-03-11 11:47:32 +0100

    Initial Commit

---
+++ MvvmRxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MvvmRxApp
+//
+//  Created by MacBook Pro on 11/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
